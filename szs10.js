7e97f941da58ca4949b6f61a6826665de014f6d9
سعيد زكريا السكرى <142688683+szs2023@users.noreply.github.com>
2023-08-31 23:07:12 +0300

    Add files via upload

---
+++ szs10.js
@@ -0,0 +1,167 @@
+// first problem solution==================================================
+// function test(x,y){
+//     //     sum =x+y
+//     //     if(sum==15){
+//     //         console.log(true)
+//     //     }
+//     //     else if (x==15 ||x==15 || y==15){
+//     //         console.log(fulse)
+//     //     }
+// }
+// test() 
+// 
+// second problem solution==================================================
+// function perfectsquare(n){
+//     if(n<=1){
+//         return true
+//     }
+//     console.log(left =1, right =n)
+//     while(left<=right){
+//         console.log(mid=left+(right-left)/2)
+//         console.log(square=mid*mid)
+//     }
+//     if(square==n){
+//         return true;
+//     }
+//     else if(square<n){
+//         left=mid+1
+//     }
+//     else{
+//         right=mid-1
+//     }
+//     return false
+    
+// }
+// perfectsquare()
+// // function test(arr){
+//      for(var i=0; i<arr.length;i++)
+//      {
+//         if (arr[i]==0)
+//         {
+//             return i+1
+//         }
+//      }   
+// }
+// console.log(test([1,0,2,3,4,5]))
+// fourth problem solution==============================================================
+//  function letter(string){
+//  var l=string;
+// if(l===string.toLowerCase()){
+//  return l.toUpperCase();
+//  }
+// else{return string.toLowerCase();}
+//  }
+//  // console.log(letter('a'));
+//  // console.log(letter('A'));
+// fifth problem solutio===============================================================
+// function sumodd(num1,num2){
+//     var sum=0;
+//     for(var i=num1;i<num2;i++){
+//         if(i%2 !=0){
+//             sum+=i
+//         }
+       
+// }
+// return sum
+
+// }
+// console.log(sumodd(10,30))
+// six problem solution====================================================================
+// function test(n){
+//     var n=+prompt("enter your nimber")
+//     if(n==4||5||6||7){
+//         console.log("it is your lucky day")
+//     }
+//     else{
+//         (n||1||2||3)
+//         console.log("it is not your lucky day")
+//     }
+// }
+// test()
+// seven problem solution========================================================================
+// function printdivors(n){
+//     for(let i=1; i<=Math.sqrt(n); i++){
+//     }
+//     if(n%i==0){
+//         if(parseInt(n/i,10)==i){
+
+//         }
+//         else{
+//             document.write(i+""+ parseInt(n/i,10)+"")
+
+//         }
+//     }
+// }
+// printdivors()
+// eight problem solution=============================================================================
+// function isprime(n){
+//     if(n==1||n==0)return false
+//     for(var i=2; i<n; i++){
+//         if(n%i==0)return false
+//     }
+//     return true
+
+//     }
+// var n=100
+// for(var i=1; i<=n; i++){
+//     if(isprime(i)){
+//         console.log(i)
+//     }
+// }
+//     isprime() 
+// ninth problem solution================================================================================
+// function test_Prime(n){
+//     var n=+prompt("enter your number")
+//     if(n==1){
+//         return false;
+//     }
+//     else if(n==2){
+//         return true
+//     }
+//     else{
+//         for(var x=2; x<n; x++){
+//         }
+//         if(n%x===0){
+//             return false
+//         }
+//     }
+//     return true
+// }
+//  console.log(test_prime()) 
+// tenth======================================================================================================
+// function test(n){
+//     var n="pum"
+//     for(i=0; i<Pum.length; i++){
+
+//     }
+    
+// }  
+// test()
+// eleven problem solution====================================================================================
+// function test(x,y){
+//     if(x*y==negative){
+//         console.log("negative")
+//     }
+//     else(x*y==positive){
+//         console.log("positive")
+//     }
+// }
+// test()
+// twelve problem solve============================================================================================
+// function final_Grade(exam,projects){
+//     finalgrade=0;
+//     if (exam>50 &&projects){
+//         finalGrade =75;
+//     }
+//     else if(exam>75&&projects){
+//         finalGrade =90;
+//     }
+//     else(exam>90&&projects){
+//         return finalGrade;
+//     }
+// }
+// final_Grad()
+
+
+
+
